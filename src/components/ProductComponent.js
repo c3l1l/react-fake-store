abb25f2f5fd7fbe8c53da2ce3d9ce4cd7f4ae090
Celil Ozturk <celil.ozturk@windowslive.com>
2023-10-24 23:37:53 +0300

    ProductComponent

---
+++ src/components/ProductComponent.js
@@ -0,0 +1,20 @@
+
+function ProductComponent({product}){
+    
+    const addBasket=()=>alert("Product added to Basket!");
+    return (
+        <>
+        <div  className="card text-center m-4" style={{width:"18rem", height:"20rem", float:"left"}}>
+                    <img src={product.image} className="card-img-top" style={{display:"block", margin:"auto",width:"100px", height:"100px"}} alt="..." />
+                    <div className="card-body">
+                        <div className="card-title" style={{fontWeight:"bold"}}>{product.title}</div>
+                        <p className="card-text text-truncate">{product.description}</p>
+                    </div>
+                    <div className="text-center" style={{fontWeight:"bold", color:"red"}}>Price:{product.price}£</div>
+                    <button onClick={addBasket} className="btn btn-outline-success">Sepete Ekle</button>
+                </div>
+        </>
+    );
+}
+
+export default ProductComponent;
--- src/pages/product.js
+++ src/pages/product.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState,useEffect } from "react";
+import ProductComponent from "../components/ProductComponent";
 function Product() {
     const [products,setProducts]=useState([]);
     const [loading,setLoading]=useState(true);
@@ -9,7 +10,7 @@ function Product() {
         .finally(()=>setLoading(false));
 
     },[]);
-    const addBasket=()=>alert("Product added to Basket!");
+   // const addBasket=()=>alert("Product added to Basket!");
     return (
         <>
   <div className="container mt-4">
@@ -19,15 +20,16 @@ function Product() {
         {
             products.map((product,index)=>(
 
-                <div  className="card text-center m-4" style={{width:"18rem", height:"20rem", float:"left"}}>
-                    <img src={product.image} className="card-img-top" style={{display:"block", margin:"auto",width:"100px", height:"100px"}} alt="..." />
-                    <div className="card-body">
-                        <div className="card-title" style={{fontWeight:"bold"}}>{product.title}</div>
-                        <p className="card-text text-truncate">{product.description}</p>
-                    </div>
-                    <div className="text-center" style={{fontWeight:"bold", color:"red"}}>Price:{product.price}£</div>
-                    <button onClick={addBasket} className="btn btn-outline-success">Sepete Ekle</button>
-                </div>
+                <ProductComponent product={product} />
+                // <div  className="card text-center m-4" style={{width:"18rem", height:"20rem", float:"left"}}>
+                //     <img src={product.image} className="card-img-top" style={{display:"block", margin:"auto",width:"100px", height:"100px"}} alt="..." />
+                //     <div className="card-body">
+                //         <div className="card-title" style={{fontWeight:"bold"}}>{product.title}</div>
+                //         <p className="card-text text-truncate">{product.description}</p>
+                //     </div>
+                //     <div className="text-center" style={{fontWeight:"bold", color:"red"}}>Price:{product.price}£</div>
+                //     <button onClick={addBasket} className="btn btn-outline-success">Sepete Ekle</button>
+                // </div>
 
             ))
         }
