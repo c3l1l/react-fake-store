c216aa7b59946d3a9cb460e874f324fd869fa3f3
Celil Ozturk <celil.ozturk@windowslive.com>
2023-10-24 23:32:52 +0300

    Product page loading

--- src/pages/product.js
+++ src/pages/product.js
@@ -2,14 +2,20 @@ import axios from "axios";
 import { useState,useEffect } from "react";
 function Product() {
     const [products,setProducts]=useState([]);
+    const [loading,setLoading]=useState(true);
     useEffect(()=>{
         axios.get("https://fakestoreapi.com/products")
-        .then((res)=>setProducts(res.data));
+        .then((res)=>setProducts(res.data))
+        .finally(()=>setLoading(false));
 
-    },[])
+    },[]);
+    const addBasket=()=>alert("Product added to Basket!");
     return (
         <>
   <div className="container mt-4">
+        {
+            loading && <p className="text-center">Loading....</p>
+        }
         {
             products.map((product,index)=>(
 
@@ -20,7 +26,7 @@ function Product() {
                         <p className="card-text text-truncate">{product.description}</p>
                     </div>
                     <div className="text-center" style={{fontWeight:"bold", color:"red"}}>Price:{product.price}£</div>
-                    <button className="btn btn-outline-success">Sepete Ekle</button>
+                    <button onClick={addBasket} className="btn btn-outline-success">Sepete Ekle</button>
                 </div>
 
             ))
